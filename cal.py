9fd866ab0ccb3e7ab92c16f2eb86a5cb4343e58f
ppf-notes <aadityasharma152007@gmail.com>
2026-04-26 00:40:37 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -2,22 +2,30 @@ import streamlit as st
 st.set_page_config(page_title="Calculater", page_icon="🌍")
 st.title("🌍 Calculater")
 operation={
-    "Sum":"+",
+    "sum":"+",
     "subtract":"-",
     "divide":"/",
     "multiply":"*"
 }
 
-target_operation = st.selectbox("operation done", list(operation.keys()))
+target_operation = st.selectbox("operation done", list(operation.keys())) 
 a = st.number_input("Enter first number")
 b = st.number_input("Enter second number")
 
 if st.button("operaTE"):
     if target_operation=="sum":
-        st.success(a+b)
+        c=a+b
+        st.success(f"result:{c}")
     elif target_operation=="subtract":
-        st.success(a-b)
+        c=a-b
+        st.success(f"result:{c}")
     elif target_operation=="divide":
-        st.success(a/b)
-    else :
-        st.success(a*b)
+        if b==0:
+            st.error("no aplicable")
+        else:
+            c=a/b
+            st.success(f"result:{c}")
+    elif target_operation == "multiply":
+        c=a*b
+        st.success(f"result:{c}")
+
